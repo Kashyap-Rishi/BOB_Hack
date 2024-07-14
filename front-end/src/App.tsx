8436f9857c30dbb472fe3b3d322fc03f22cadba7
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2024-07-14 16:18:09 +0530

    Json converter feature added

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DashboardLayout from './components/Dashboard/DashboardLayout';
+import FileUpload from './components/Dashboard/FileUpload';
+import FileContentViewer from './components/FileContentViewer';
+import Signup from './components/Signup';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const storageAccountUrl = "https://storagecentercsvfiles.blob.core.windows.net";
+  const sasToken = "sp=racwdli&st=2024-07-14T04:07:38Z&se=2024-07-14T12:07:38Z&sv=2022-11-02&sr=c&sig=qZY2UqGi0S1J2Qb%2BCRzLmmSJB3CyQ3G9iU6qPE66Q38%3D";
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="file-upload" element={<FileUpload storageAccountUrl={storageAccountUrl} sasToken={sasToken} />} />
+         
+        </Route>
+        <Route path="file-viewer/:fileName" element={<FileContentViewer />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+
+    <GoogleOAuthProvider clientId="158478297408-gevob1skc53nfic5u33qd9nguctbmkq8.apps.googleusercontent.com">
     <App />
-  </React.StrictMode>,
+    </GoogleOAuthProvider>
+  
 )
